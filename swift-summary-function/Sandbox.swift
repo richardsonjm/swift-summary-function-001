28fe888ef128bd4f0189e7ddc6f82d9e12b97533
Tim Clem <tim.clem@gmail.com>
2015-11-30 11:17:35 -0500

    Remove solution

--- swift-summary-function/Sandbox.swift
+++ swift-summary-function/Sandbox.swift
@@ -7,8 +7,3 @@ import Foundation
 *  Write your function here!
 
 */
-
-func summaryForCourse(course: String, leadInstructor: String, language: String, students: Int) -> String {
-    let summary = "In the \(course) class at the Flatiron School there are \(students) students learning \(language) from \(leadInstructor)."
-    return summary
-}
