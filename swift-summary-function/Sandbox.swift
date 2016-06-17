b5e1944b53bbcbb3582230c1bc3e95f96e0a46a9
John Richardson <richardsonjm@gmail.com>
2016-06-17 17:23:38 -0400

    Course list

--- swift-summary-function/Sandbox.swift
+++ swift-summary-function/Sandbox.swift
@@ -2,8 +2,9 @@
 
 import Foundation
 
-/**
+//In the Mobile Development with iOS class at the Flatiron School there are 20 students learning Swift from Tim Clem.
 
-*  Write your function here!
 
-*/
+func summaryForCourse(course: String, leadInstructor: String, language: String, students: Int) -> String {
+    return "In the \(course) class at the Flatiron School there are \(students) students learning \(language) from \(leadInstructor)."
+}
